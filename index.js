6d01bb41288de648c95cf88167fc2c2f8a16f155
Eren Macit <ernmctt@gmail.com>
2023-03-01 16:07:14 +0300

    v.0

---
+++ index.js
@@ -0,0 +1,11 @@
+
+let toTop = document.getElementById('scrl')
+toTop.addEventListener('click',function()
+{
+    window.scrollTo({
+        top:0,
+        left:0,
+        behavior:'smooth'
+    });
+
+})
