465ce814c1496294e80ba5ad97e9bf4b3eb46372
Eren Macit <ernmctt@gmail.com>
2023-03-13 05:33:43 +0300

    news page and news api test

--- index.js
+++ index.js
@@ -12,3 +12,42 @@ let year = new Date();
 year.getFullYear();
 document.getElementById("copy").innerHTML = `Copyright &copy; ${year.getFullYear()} Seremköy v0.5 BETA. Created by rnm - bablock`
 
+// 2a549216ecee4d72bf04a04531f22452  news api key
+
+
+fetch("https://newsapi.org/v2/everything?q=edirne&apiKey=2a549216ecee4d72bf04a04531f22452")
+.then((response) => response.json())
+.then( (data) => {
+    console.log(data.articles)
+    let news = data.articles
+    const newsAdd = document.getElementById("contCardMain")
+    news.forEach(element => {
+
+            let author = element.author
+            let title = element.title
+            let description = element.description
+            let publishedAt = element.publishedAt
+            let url = element.url
+            let urlToImage = element.urlToImage
+
+            if(author === null){
+                author = "Bulunamadı"
+            }
+            console.log(author)
+
+            let newsTemplate =
+            `
+            <div class="card">
+          <div class="photoDiv">
+            <img class="news-img" src="${urlToImage}" alt="">
+            </div>
+                <div class="newsContent">
+                    <h1>${title}</h1>
+                    <p>${description}</p>
+                    <a href="${url}" class="newsLink" target="_blank">Habere Git</a>
+                </div>
+                </div>
+            `
+            newsAdd.innerHTML += newsTemplate
+    });
+})
