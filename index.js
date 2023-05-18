23198e3dfd1a38a30873e8652d4785916b37becf
Eren Macit <ernmctt@gmail.com>
2023-05-18 15:36:43 +0300

    feat: weather end

--- index.js
+++ index.js
@@ -1,47 +1,70 @@
 //Scroll Back To Top -START-
-let toTop = document.getElementById('scrl') //to top button
-toTop.addEventListener('click', function () {
-    window.scrollTo({
-        top: 0, //Top poz
-        left: 0, //Left poz
-        behavior: 'smooth' //scroll type
-    });
+let toTop = document.getElementById('scrl') //to top butonunun seçilmesi
+toTop.addEventListener('click', function () { //Butona click eventi
+  window.scrollTo({ //Tıklandığında yönlendirecek fonksiyon
+    top: 0, //Top konumu
+    left: 0, //Left konumu
+    behavior: 'smooth' //Hareket şekli
+  });
 })
 //Scroll Back To Top -END-
 
-//Copyright Date -START-
+//Copyright için tarih -START-
 let year = new Date();
-year.getFullYear(); //year
-//render year
+year.getFullYear(); //Yılın seçilmesi
+//Yılın footer'a yazdırılması
 document.getElementById("copy").innerHTML = `Tüm hakları saklıdır. &copy; ${year.getFullYear()} Seremköy v.0.5 <br><span class="cr"> </span><br><span class="dvlpr">Creators rnm💀 - fatih nayir🐦</span>`
-//Copyright Date -END-
+//Copyright için tarih -END-
 
 
 
 
 
-async function test() {
-    try {
-      // API isteği atılır
-      const response = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=41.262625&lon=26.710540&lang=tr&appid=d25e485f9a40cf3edd8d1f42ed8fab4e");
-  
-      // İstek sonucu alınır ve JSON formatına çevrilir
-      const data = await response.json();
-  
+const weatherCont = document.getElementById("weather-cont")
+
+async function weatherApi() {
+  try {
+    // API isteği atılır
+    const response = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=41.262625&lon=26.710540&units=metric&lang=tr&appid=d25e485f9a40cf3edd8d1f42ed8fab4e");
+
+    // İstek sonucu alınır ve JSON formatına çevrilir
+    const data = await response.json();
+
+
+    if (response.ok) {
+      // Yanıt başarılı
+
       // Alınan veriler konsola yazdırılır
       console.log(data);
-  
+
+      //Alınan veriler değişkene atanır
+      const weatherMain = data.weather[0].main
+      const weatherIcon = data.weather[0].icon
+      const weatherTemp = data.main.temp
+
+      let weatherTemplate = `
+          <div class="temp">
+            <img src="https://openweathermap.org/img/wn/${weatherIcon}@2x.png">   
+            <span class="temp">${weatherTemp}°</span>
+          </div>
+          <div>
+            <span>Uzunköprü</span>
+          </div>
+        `
+
+
+        weatherCont.innerHTML = weatherTemplate;
+    } else {
       // Yanıt başarılı değilse, bir hata fırlatılır
-      if (!response.ok) {
-        throw new Error("API isteği başarısız oldu");
-      }
-    } catch (error) {
-      // Hata oluştuğunda konsola yazdırılır
-      console.log(error);
+      throw new Error("API isteği başarısız oldu")
     }
+  } catch (error) {
+    // Hata oluştuğunda konsola yazdırılır
+    console.log(error);
   }
-  
-test();
+}
+
+weatherApi();
 
 
 
