63133770ea9a2e3e8ab3a21b560af2a4d000ace6
Fatih Nayir <fatih-bjk22@hotmail.com>
2023-05-21 03:52:36 +0300

    before deploy changes

---
+++ haber.js
@@ -0,0 +1,71 @@
+"use-strict"
+
+const RSS_URL = "https://www.mynet.com/haber/rss/kategori/guncel/";
+// // const PROXY_URL = "https://api.allorigins.win/raw?url=" + encodeURIComponent(RSS_URL);
+const newsAddDiv = document.getElementById("contCardMain") //Haberlerin ekleneceği div
+let count = 12
+let sliceStart = 0
+let sliceEnd = count
+
+async function mynetRSS() {
+    try {
+        newsAddDiv.innerHTML = "";
+        // rssCount = count || 10
+        const response = await fetch(RSS_URL)
+        const xmlData = await response.text();
+        const parser = new DOMParser();
+        const data = parser.parseFromString(xmlData, "text/xml")
+        const items = Array.from(data.querySelectorAll("item"))
+        const filteredItems = items.slice(sliceStart, sliceEnd)
+        console.log(items.length)
+
+        filteredItems.forEach(item => {
+            const title = item.querySelector("title").textContent;
+            const link = item.querySelector("link").textContent;
+            const image = item.querySelector("img640x360").textContent;
+            const date = item.querySelector("pubDate").textContent;
+            const desc = item.querySelector("description").textContent;
+            const sourceStartIndex = 12;
+            const sourceEndIndex = link.indexOf(".com");
+            const source = link.slice(sourceStartIndex, sourceEndIndex);
+
+            let newsTemplate =
+                `
+                            <div class="card">
+                                <img class="news-img" src="${image}" alt="">
+                                <div class="newsContent">
+                                <h1 class="bold">${title}</h1>
+                                <div id="hbr"> <p>${desc}</p> </div>
+                                
+                                <p><span class="bold">Yayınlanma Tarihi: </span>${date}</p>
+                                <p><span class="bold">Kaynak: </span>${source}</p>
+                                 <a href="${link}" target="_blank" class="newsLink">Habere Git</a>
+                                </div>
+                            </div>
+                        `
+            newsAddDiv.innerHTML += newsTemplate //Haber template'inin eklenmesi
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const nextNews = document.getElementById("next")
+nextNews.addEventListener("click", () => {
+    count = count + 12
+    sliceStart = sliceStart + 12
+    sliceEnd = sliceEnd + 12
+    mynetRSS(count)
+})
+
+const forwardNews = document.getElementById("forward")
+forwardNews.addEventListener("click", () => {
+    if (count !== 12) {
+        count = count - 12
+        sliceStart = sliceStart - 12
+        sliceEnd = sliceEnd - 12
+        mynetRSS(count)
+    }
+})
+
+mynetRSS(count);
--- index.js
+++ index.js
@@ -1,149 +1,109 @@
 //Scroll Back To Top -START-
 let toTop = document.getElementById('scrl') //to top butonunun seçilmesi
 toTop.addEventListener('click', function () { //Butona click eventi
-    window.scrollTo({ //Tıklandığında yönlendirecek fonksiyon
-        top: 0, //Top konumu
-        left: 0, //Left konumu
-        behavior: 'smooth' //Hareket şekli
-    });
+  window.scrollTo({ //Tıklandığında yönlendirecek fonksiyon
+    top: 0, //Top konumu
+    left: 0, //Left konumu
+    behavior: 'smooth' //Hareket şekli
+  });
 })
 //Scroll Back To Top -END-
+
+/*MOBILE MENU AREA START*/ 
+function MobileNav(){
+ 
+  const Mtoggle = document.getElementById("mobile-menu");
+  const mTog = document.getElementById("mTog");
+  const togBtn = document.getElementById("mobile-toggle");
+  
+  document.addEventListener('click', function(e) {
+    if (!Mtoggle.contains(e.target) && e.target != mTog ) {
+        Mtoggle.style.transform = "translateX(1000px)";
+        mTog.classList.add('fa-ellipsis');
+        mTog.classList.remove('fa-circle-xmark');
+        // togBtn.style.left = "5%";
+    }
+});
+
+  if (Mtoggle.style.transform === "translateX(0px)" ){
+      Mtoggle.style.transform = "translateX(1000px)";
+      mTog.classList.add('fa-ellipsis');
+      mTog.classList.remove('fa-circle-xmark');
+      // togBtn.style.left = "5%";
+      
+
+  } else{
+      Mtoggle.style.transform = "translateX(0px)"     
+      mTog.classList.add('fa-circle-xmark');
+      mTog.classList.remove('fa-ellipsis');
+      togBtn.style.transition = "500ms";
+      // togBtn.style.left = "37%";
+  }   
+}
+/*MOBILE MENU AREA END*/ 
+const weatherCont = document.getElementById("weather-cont")
+const mobileWeather = document.querySelector('.mobile-weather')
+async function weatherApi() {
+  try {
+    // API isteği atılır
+    const response = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=41.262625&lon=26.710540&units=metric&lang=tr&appid=d25e485f9a40cf3edd8d1f42ed8fab4e");
+
+    // İstek sonucu alınır ve JSON formatına çevrilir
+    const data = await response.json();
+
+
+    if (response.ok) {
+      // Yanıt başarılı
+
+      // Alınan veriler konsola yazdırılır
+      console.log(data);
+
+      //Alınan veriler değişkene atanır
+      const weatherMain = data.weather[0].main
+      const weatherIcon = data.weather[0].icon
+      const weatherTemp = data.main.temp
+
+      let weatherTemplate = `
+          <div class="temp">
+            <img src="https://openweathermap.org/img/wn/${weatherIcon}@2x.png">   
+            <span class="temp">${weatherTemp}°</span>
+          </div>
+          <div>
+            <span>Uzunköprü</span>
+          </div>
+        `
+
+
+        weatherCont.innerHTML = weatherTemplate;
+        mobileWeather.innerHTML = weatherTemplate;
+    } else {
+      // Yanıt başarılı değilse, bir hata fırlatılır
+      throw new Error("API isteği başarısız oldu")
+    }
+  } catch (error) {
+    // Hata oluştuğunda konsola yazdırılır
+    console.log(error);
+  }
+}
+
+weatherApi();
+
+
 
 //Copyright için tarih -START-
 let year = new Date();
 year.getFullYear(); //Yılın seçilmesi
 //Yılın footer'a yazdırılması
-document.getElementById("copy").innerHTML = `Tüm hakları saklıdır. &copy; ${year.getFullYear()} Seremköy v.0.5 <br><span class="cr"> </span><br><span class="dvlpr">Creators rnm💀 - fatih nayir🐦</span>`
+document.getElementById("copy").innerHTML = `Tüm hakları saklıdır. &copy; ${year.getFullYear()} Seremköy v. 1 <span class="dvlpr">Developed By <a href="https://macidko.netlify.app/" target="_blank">Eren</a> & <a href="https://fatih-nayir.netlify.app/" target="_blank">Fatih</a></span>`
 //Copyright için tarih -END-
 
-// 2a549216ecee4d72bf04a04531f22452  news api key
-let length = 100 //Api'dan gelen haber sayısının azaltılması için
-let index = 0; //for döngüsünün değişkeni
 
-// window.addEventListener("load", (event) => {
-//     haberCek();
-// });
-
-
-// function haberCek() {
-//     console.log("Fonksiyon içi length: ", length)
-//     console.log("Fonksiyon içi index: ", index)
-
-fetch("https://newsapi.org/v2/everything?q=edirne&apiKey=2a549216ecee4d72bf04a04531f22452") //Api'a istek atılan kısım
-    .then((response) => response.json()) //Api'dan gelen json yanıtının dönüştürülmesi
-    .then((data) => { //Dönüştürülen data'nın kullanıldığı kısım
-        let news = data.articles //Api'da haberlerin içerisinde bulunduğu kısım
-        const newsAddDiv = document.getElementById("contCardMain") //Haberlerin ekleneceği div'in seçilmesi
-
-        for (index = 0; index < length; index++) { //Api'dan gelen haber sayısının(100) azaltılması için
-
-            let author = news[index].author //Kaynak
-            let title = news[index].title //Başlık
-            let description = news[index].description //Açıklama
-            let publishedAt = news[index].publishedAt.slice(0, 10).split("-") //Tarih ancak iso formatında. O yüzden array'e dönüştürüldü.
-            let publishedAtReverse = publishedAt[2] + "-" + publishedAt[1] + "-" + publishedAt[0] //Dönüştürülen tarih ters şekilde yazdırıldı.
-            let url = news[index].url //Haber url'i
-            let urlToImage = news[index].urlToImage //Haber görseli
-
-            if (author === null) { //kaynak "null" olduğu zaman
-                let dotcomPart = null // url'deki ".com" kısmının indexi için
-                url.indexOf(".com") > -1 ? dotcomPart = url.indexOf(".com") : dotcomPart = url.indexOf(".de") //yabancı kaynakların ".com" içermemesinden dolayı
-                author = url.substring(12, dotcomPart) //Null gelen kaynak değerinin url üzerinden filtreleyerek bulunması
-            }
-            //Haberlerin eklenmesi için html template'i
-            let newsTemplate =
-                `
-                    <div class="card">
-                        <img class="news-img" src="${urlToImage}" alt="">
-                        <div class="newsContent">
-                        <h1 class="bold">${title}</h1>
-                        <p>${description}</p>
-                        <p><span class="bold">Yayınlanma Tarihi: </span>${publishedAtReverse}</p>
-                        <p><span class="bold">Kaynak: </span>${author}</p>
-                        <button class="newsLink" type="button"> <a href="${url}" target="_blank">Habere Git</a></button>
-                        </div>
-                    </div>
-                `
-            newsAddDiv.innerHTML += newsTemplate //Haber template'inin eklenmesi
-
-            // let artiBest = document.getElementById("artiBes") 
-            // artiBest.addEventListener("click", () => {
-            //     let x = document.querySelectorAll(".card")
-            //     x.forEach(element => {
-            //         element.remove();
-            //     })
-            //     length = length + 5
-            //     // index = index + 5
-            //     console.log("Click içi length: ", length)
-            //     console.log("Click içi index: ", index)
-
-            //     haberCek()
-            // })
-        }
-        // arr.forEach(element => {
-
-        // });
-    })
+// const mobileNav = document.querySelector(".menu-list")
+// const hiddenLength = document.querySelectorAll(".hidden").length
 
 
 
+//EMAILJS BEGIN
 
 
-
-
-
-
-// }
-
-// window.onload = (event) => {
-//     haberCek()
-
-// };
-
-// let artiBest = document.getElementById("artiBes")
-// artiBest.addEventListener("click", () => {
-//     length += 1;
-//     console.log(length)
-//     haberCek()
-// })
-
-
-
-
-
-
-
-
-// const dolares = document.getElementById("dollaresS")
-// const yuro = document.getElementById("yuro")
-// const bulgardaramcigi = document.getElementById("bulgaramcigi")
-
-
-//     fetch("https://api.exchangerate.host/latest?base=TRY&&symbols=BGN,USD,EUR")
-//     .then( (response) => response.json())
-//     .then( (data) => {
-//         dolares.textContent = "TRY-DOLAR" + data.rates.USD
-//         yuro.textContent = "TRY-EURO" + data.rates.EUR
-//         bulgardaramcigi.textContent = "TRY-Bulgar amcigi" + data.rates.BGN
-//     } )
-
-
-
-
-
-// // IYZ5MS47Y407IYVV alphavantage api key
-// https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.json
-// https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/2022-11-24/currencies/usd.json
-
-
-//MOBILE NAV BEGIN
-
-const hamburgerButton = document.getElementById("hamburger")
-const mobileNav = document.querySelector(".menu-list")
-const hiddenLength = document.querySelectorAll(".hidden").length
-
-// hamburgerButton.addEventListener("click", function () {
-//     mobileNav.classList.toggle("hidden")
-// })
-
-//MOBILE NAV END
+//EMAILJS END
